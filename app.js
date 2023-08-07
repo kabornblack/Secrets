bfb1cdd05bcc5b6e81f8540027f8865fec212eff
kabornblack <shaibu.kabiru@email.com>
2023-08-07 08:52:24 +0300

    Added environment vars

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -30,8 +31,7 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = "ThisIsMyLittleSecrets.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
